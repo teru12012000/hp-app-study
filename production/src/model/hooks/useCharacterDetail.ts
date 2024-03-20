c6728e51de7af9d2084796fd1cb01667d7d93d16
teru12012000 <sion2769@gmail.com>
2024-03-21 03:05:27 +0900

    fix: state

--- production/src/model/hooks/useCharacterDetail.ts
+++ production/src/model/hooks/useCharacterDetail.ts
@@ -3,11 +3,11 @@ import useSWR from "swr"
 import { character } from "../fetcher"
 
 export const useCharacterDetail = (id: string) => {
-    const { data } = useSWR(process.env.NEXT_PUBLIC_URL2, (url) =>
+    const { data, isLoading } = useSWR(process.env.NEXT_PUBLIC_URL2, (url) =>
         character(id),
     )
 
     const resData = data ?? null
 
-    return { resData }
+    return { resData, isLoading }
 }
--- production/src/view/RCC/RCCCharacterDetail/RCCCharacterDetail.tsx
+++ production/src/view/RCC/RCCCharacterDetail/RCCCharacterDetail.tsx
@@ -1,24 +1,26 @@
 "use client"
 
 import { useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { CharacterDetailTemplate } from "@/components/layout/characterDetailTemplate/characterDetailTemplate"
 import { useCharacterDetail } from "@/model/hooks"
 
 const RCCCharacterDetail = () => {
     const searchParam = useSearchParams()
     const id = searchParam.get("id")
-    const [isDifferent, setIsDifferent] = useState<boolean>(false)
+    const [isDifferent, setIsDifferent] = useState<boolean>(true)
 
-    const { resData } = useCharacterDetail(id as string)
+    const { resData, isLoading } = useCharacterDetail(id as string)
 
     /**
      * ここはキャッシュが効いちゃってるのでバグが起こります。
      * 検証したければこの部分を削除してみてください！
      */
     useEffect(() => {
-        setIsDifferent(id !== resData?.id)
-    }, [resData])
+        console.log(id, resData)
+        if (resData !== null && id !== null) setIsDifferent(id !== resData.id)
+        else setIsDifferent(true)
+    }, [resData, id])
 
     return <CharacterDetailTemplate data={resData} isLoading={isDifferent} />
 }
